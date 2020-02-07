f55faac3270e1681921f985037a44da4a176a2ad
Loic Quentin <lquentin@aquaray.com>
2020-02-07 09:18:18 +0000

    Add Submodule Dotbot and reorganize dotfile structure

--- create_symlink.sh
+++ create_symlink.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir ~/.dotfile.old
-mv .bashrc .bash_logout ~/.dotfile.old/
+mv ~/.bashrc ~/.bash_logout ~/.dotfile.old/
 
 for i in $(find "$(pwd)" -type f | grep -v ".git" | grep -v $0 | grep -v README); do
 	ln -s -t ~ $i
