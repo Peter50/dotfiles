bf119f3bbc2c30c6b9c813ad0d8701a381d6113b
Loic Quentin <lquentin@aquaray.com>
2020-02-07 09:21:09 +0000

    Remove legacy file

--- create_symlink.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir ~/.dotfile.old
-mv ~/.bashrc ~/.bash_logout ~/.dotfile.old/
-
-for i in $(find "$(pwd)" -type f | grep -v ".git" | grep -v $0 | grep -v README); do
-	ln -s -t ~ $i
-done
